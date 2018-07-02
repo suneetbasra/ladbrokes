a3680d2e429e4ea8bfc128d80627e8573ce10224
suneetbasra <suneet023@yahoo.com>
2018-07-02 19:53:15 +0100

    changing waittime test
    testing

--- src/test/java/com/QA/step_definations/moneysuper.java
+++ src/test/java/com/QA/step_definations/moneysuper.java
@@ -57,7 +57,7 @@ public class moneysuper extends opendriver {
     @Then("^I should get an error message as \"([^\"]*)\" in moneyweb$")
     public void iShouldGetAnErrorMessageAsInMoneyweb(String error) throws Throwable {
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         driver.findElement(By.xpath(".//*[@id='signInForm']/div/div[2]/div/div[5]/div")).getText();
         driver.findElement(By.xpath(".//*[@id='signInForm']/div/span[2]/span/span/div[5]/div")).getText();
 //        github sample
